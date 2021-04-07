82c21cc2b7865367bcd04a13dcac3cb174b18f7a
vamsee9 <4n0n5ur7@protonmail.com>
2021-04-07 22:27:12 +0530

    added 3rd route

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from flask import Flask
 app = Flask (__name__)
 
 
-@app.route('/<name>')
-def index(name):
-    return '<h1>Hello{}!<h1>'.format(name)
+@app.route('/')
+def index():
+    return "hello world"
 @app.route("/saikiran")
 def saikiran():
     return"Hello, saikiran!"
+@app.route("/python_Flask")
+def python_Flask():
+    return "2nd elective is, python_Flask"
