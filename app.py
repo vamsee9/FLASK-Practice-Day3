395e479e453d0c6287bffcf3dfdc98654497910e
vamsee9 <4n0n5ur7@protonmail.com>
2021-04-08 02:11:40 +0530

    INHERITANCE INCLUDED

--- app.py
+++ app.py
@@ -1,41 +1,22 @@
-#import datetime
-from flask import Flask, render_template
-
-
-app = Flask (__name__)
-
+from flask import Flask, render_template, request
+app = Flask(__name__)
 @app.route("/")
 def index():
-    names = ["sai", "kiran", "warmsiee", "nowyah!!"]
-    return render_template("index.html", names=names)
-# @app.route('/')
-# def index():  
-#      now = datetime.datetime.now()
-#      new_year = now.month==1 and now.day ==1
-#      #new_year = True
-#      text = "yes" if new_year else ""
+    return render_template("index.html")
+@app.route("/hello", method=["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html", name=name)
 
-#      return render_template("index.html",new_year=new_year)
 
-#     headline = "hello, saikiran!!"             #
-#     return render_template("index.html", headline=headline)
 
-# @app.route("/bye")
-# def bye():
-#     headline = "Good bye!"
-#     return render_template("index.html", headline=headline)
-     
+# # 
+# from flask import Flask,render_template
+# app = Flask(__name__)
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+# @app.route("/more")
+# def more():
+#     return render_template("more.html")
 
-   
-   
-    #return "hello world"
-# # @app.route("/saikiran")
-# # def saikiran():
-# #     return"Hello, saikiran!"
-# # @app.route("/python_Flask")
-# # def python_Flask():
-# #     return "2nd elective is, python_Flask"
-# @app.route("/<string:name>")# will print message for string what we are giving 
-# def hello(name):
-#     name = name.capitalize()
-#     return f"<h1>Hello, {name}!</h1>"
