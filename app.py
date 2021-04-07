a4247bd7eb8973fca9ea6733f3d2a2261a177a5d
vamsee9 <4n0n5ur7@protonmail.com>
2021-04-07 23:03:35 +0530

    added HTML template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask (__name__)
@@ -6,14 +6,15 @@ app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
-# @app.route("/saikiran")
-# def saikiran():
-#     return"Hello, saikiran!"
-# @app.route("/python_Flask")
-# def python_Flask():
-#     return "2nd elective is, python_Flask"
-@app.route("/<string:name>")# will print message for string what we are giving 
-def hello(name):
-    name = name.capitalize()
-    return f"<h1>Hello, {name}!</h1>"
+    return render_template("index.html")
+    #return "hello world"
+# # @app.route("/saikiran")
+# # def saikiran():
+# #     return"Hello, saikiran!"
+# # @app.route("/python_Flask")
+# # def python_Flask():
+# #     return "2nd elective is, python_Flask"
+# @app.route("/<string:name>")# will print message for string what we are giving 
+# def hello(name):
+#     name = name.capitalize()
+#     return f"<h1>Hello, {name}!</h1>"
